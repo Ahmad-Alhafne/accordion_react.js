2604b53064f56b70d072d9e051f065ecd3fb5247
Ahmad-Alhafne <ahmad4732916@gmail.com>
2024-04-30 16:40:02 +0300

    first react app commit

---
+++ setup/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+export default function App() {
+  return (
+    <>
+      <List />
+    </>
+  )
+}
+
---
+++ setup/src/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function List() {
+  return (
+    <>
+      <h2>list component Free</h2>
+    </>
+  )
+}
+
+
